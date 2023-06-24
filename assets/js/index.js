fa2e8fff594591efe759ac2fcefdd5c125fe2564
rettouseisama <rettouseisama@gmail.com>
2023-06-24 11:14:32 -0500

    Minor Change

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
 import { SOCIALMEDIA } from "./social-links";
 
+if('serviceWorker' in navigator){
+	navigator.serviceWorker.register('./sw.js')
+	.then(reg=>console.log('Success',reg))
+	.catch(err=>console.warn('Error',err))
+}
+
 document.addEventListener('DOMContentLoaded',ev=>{
     SOCIALMEDIA()
 })
