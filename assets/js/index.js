4302fac060d41a426ba67805d0e13f231d12f197
rettouseisama <rettouseisama@gmail.com>
2023-06-24 23:30:25 -0500

    Minor Change

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,17 @@ import { SOCIALMEDIA } from "./social-links.js";
 
 document.addEventListener('DOMContentLoaded',ev=>{
     SOCIALMEDIA()
+
+    document.addEventListener('click',ev=>{
+        const TARGET = ev.target
+        if(TARGET.matches('#toggle__social__btn, #toggle__social__btn *')){
+            const 
+                $SOCIALMEDIA_HIDDEN = document.getElementById('social__hidden'),
+                $BTN_USE = document.getElementById('change__icon'),
+                CURRENT_ICON = $BTN_USE.getAttribute('href')
+
+            $SOCIALMEDIA_HIDDEN.classList.toggle('hidden')
+            CURRENT_ICON === '#caret__right' ? $BTN_USE.setAttribute('href', '#caret__left') : $BTN_USE.setAttribute('href', '#caret__right')
+        }
+    })
 })
