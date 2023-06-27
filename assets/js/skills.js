704a5dcbb58798751f5f631f4bc8b4f8310692db
jonvemo <rettouseisama@gmail.com>
2023-06-27 15:54:52 -0500

    Test

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -2,35 +2,13 @@ export function SKILLS() {
     return fetch('/data/skills.json')
     .then(response => response.json())
     .then(data => {
-        const $FRONTEND = document.getElementById('frontend')
-        const FRONTEND_SKILLS = data.frontend
-        const FRONTEND_NAME = FRONTEND_SKILLS.map(skill => skill.name)
-        const FRONTEND_ICON = FRONTEND_SKILLS.map(skill => skill.icon)
-        const $TEMPLATE_FRONTEND = document.getElementById('template__frontend').content
 
-        const $DESIGN = document.getElementById('design')
-        const DESIGN_SKILLS = data.frontend
-        const DESIGN_NAME = FRONTEND_SKILLS.map(skill => skill.name)
-        const DESIGN_ICON = FRONTEND_SKILLS.map(skill => skill.icon)
-        const $TEMPLATE_DESIGN = document.getElementById('template__design').content
+        const NEW_JSON = data
+        const NEW_PARSE = JSON.parse(NEW_JSON)
 
-        const $FRAGMENT = document.createDocumentFragment()
-        const insertSkills = (skill,icon,template,container) => {
-            
-            skill.forEach( (el) => {
-                
-                template.querySelector('use').setAttribute('href', `#${el.toLowerCase()}`)
-                template.querySelector('span').textContent = `${el}`
-        
-                let $clone = document.importNode(template, true)
-                $FRAGMENT.appendChild($clone)
-            })
-            
-            container.replaceChildren($FRAGMENT)
-        }
-        
-        insertSkills(FRONTEND_NAME,FRONTEND_ICON,$TEMPLATE_FRONTEND,$FRONTEND)
-        insertSkills(DESIGN_NAME,DESIGN_ICON,$TEMPLATE_DESIGN,$DESIGN)
+        console.log(NEW_PARSE.frontend.name)
+
+
         
     })
     .catch(error => {
