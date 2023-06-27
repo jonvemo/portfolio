51b3ecad483cbb040f89d3b6df99a613e06baf5f
jonvemo <rettouseisama@gmail.com>
2023-06-27 15:44:43 -0500

    Test

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -3,12 +3,17 @@ export function SKILLS() {
     .then(response => response.json())
     .then(data => {
         const $FRONTEND = document.getElementById('frontend')
-        
         const FRONTEND_SKILLS = data.frontend
         const FRONTEND_NAME = FRONTEND_SKILLS.map(skill => skill.name)
         const FRONTEND_ICON = FRONTEND_SKILLS.map(skill => skill.icon)
-
         const $TEMPLATE_FRONTEND = document.getElementById('template__frontend').content
+
+        const $DESIGN = document.getElementById('design')
+        const DESIGN_SKILLS = data.frontend
+        const DESIGN_NAME = FRONTEND_SKILLS.map(skill => skill.name)
+        const DESIGN_ICON = FRONTEND_SKILLS.map(skill => skill.icon)
+        const $TEMPLATE_DESIGN = document.getElementById('template__design').content
+
         const $FRAGMENT = document.createDocumentFragment()
         const insertSkills = (skill,icon,template,container) => {
             
@@ -25,6 +30,7 @@ export function SKILLS() {
         }
         
         insertSkills(FRONTEND_NAME,FRONTEND_ICON,$TEMPLATE_FRONTEND,$FRONTEND)
+        insertSkills(DESIGN_NAME,DESIGN_ICON,$TEMPLATE_DESIGN,$DESIGN)
         
     })
     .catch(error => {
