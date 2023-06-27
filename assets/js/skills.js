8b2e95a9d4b9267a13b094355b6e8c490d972ef0
jonvemo <rettouseisama@gmail.com>
2023-06-27 16:02:08 -0500

    Test

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -2,12 +2,17 @@ export function SKILLS() {
     return fetch('/data/skills.json')
     .then(response => response.json())
     .then(data => {
+        let json = '{"nombre": "Juan", "edad": 25, "ciudad": "Madrid"}';
+        let objeto = JSON.parse(json);
+        console.log(objeto.nombre)
+
         console.log(data)
         console.log(data.frontend)
         
         const NEW_PARSE = JSON.parse(data)
 
         console.log(NEW_PARSE.name)
+
 
 
         
