d9c7fa32a1bc85b2e33b00cf83d040a859b4fe98
Jonvemo <rettouseisama@gmail.com>
2024-08-01 23:16:50 -0500

    fix(icons): app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,38 @@
 const APP = {
-    SW: null,
-    isOnline: 'onLine' in navigator && navigator.onLine,
-    init() {
-        APP.registerSW()
+  SW: null,
+  isOnline: 'onLine' in navigator && navigator.onLine,
+  init() {
+    APP.registerSW()
 
-        window.addEventListener('offline', APP.offline)
-        window.addEventListener('online', APP.online)
-    },
-    registerSW(){
-        if('serviceWorker' in navigator){
-            navigator.serviceWorker.register('/sw.js')
-            .then( reg=>console.log(`Service Worker Registered in %cscope: ${reg.scope}`,'font-weight:bold;') )
-            .catch( err=>console.warn(`%cError ${err}`, 'color: red; font-size: 1.5rem; font-weight: bold;') )
-        }
-    },
-    offline(){
-        const $PROBABLY = document.getElementById('isOnOff')
-        $PROBABLY.classList.add('isOnOff')
-        $PROBABLY.querySelector('use').setAttribute('href', `#offline`)
-
-        setTimeout(() => {
-            $PROBABLY.classList.remove('isOnOff');
-        }, 10000);
-        
-    },
-    online(){
-        const $PROBABLY = document.getElementById('isOnOff')
-        $PROBABLY.classList.add('isOnOff')
-        $PROBABLY.querySelector('use').setAttribute('href', `#online`)
-
-        setTimeout(() => {
-            $PROBABLY.classList.remove('isOnOff');
-        }, 10000);
+    window.addEventListener('offline', APP.offline)
+    window.addEventListener('online', APP.online)
+  },
+  registerSW() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log(`Service Worker Registered in %cscope: ${reg.scope}`, 'font-weight:bold;'))
+        .catch(err => console.warn(`%cError ${err}`, 'color: red; font-size: 1.5rem; font-weight: bold;'))
     }
+  },
+  offline() {
+    const $PROBABLY = document.getElementById('isOnOff')
+    $PROBABLY.classList.add('isOnOff')
+    $PROBABLY.querySelector('use').setAttribute('href', `/assets/icon/icons.svg#offline`)
+
+    setTimeout(() => {
+      $PROBABLY.classList.remove('isOnOff');
+    }, 10000);
+
+  },
+  online() {
+    const $PROBABLY = document.getElementById('isOnOff')
+    $PROBABLY.classList.add('isOnOff')
+    $PROBABLY.querySelector('use').setAttribute('href', `/assets/icon/icons.svg#online`)
+
+    setTimeout(() => {
+      $PROBABLY.classList.remove('isOnOff');
+    }, 10000);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', APP.init)
