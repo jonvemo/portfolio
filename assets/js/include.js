4ada07eb83e90ef71726a965e8ab7b427f0a7c19
Jonvemo <rettouseisama@gmail.com>
2024-09-23 16:48:25 -0500

    feat(XHR to Fetch): Fetch API instead XHR

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,20 +1,18 @@
-export const getHTML = (url, options) => {
-    const XHR = new XMLHttpRequest();
-    const { success, error } = options;
+export const getHTML = async (url, options) => {
+  const { success, error } = options;
 
-    XHR.addEventListener('readystatechange',e=>{
-        if(XHR.readyState !==4) return
-        if(XHR.status >= 200 && XHR.status < 300){
-            const html = XHR.responseText
-            success(html)
-        } else {
-            const message = XHR.statusText || 'Error'
-            error(`Error ${XHR.status}: ${message}`)
-        }
-    })
+  try {
+    const response = await fetch(url);
 
-    XHR.open('GET',url)
-    XHR.setRequestHeader('Content-type','text/html; charset=utf-8')
-    XHR.send()
-}
-  
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    const html = await response.text();
+    success(html);
+
+  } catch (err) {
+    error(err.message);
+  }
+};
+
